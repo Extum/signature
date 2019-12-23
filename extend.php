c23e9cb6daec4ea5c509f7769985a2588216d0fc
katosdev <7927609+katosdev@users.noreply.github.com>
2019-12-23 16:59:58 +0000

    Update to beta 10

--- extend.php
+++ extend.php
@@ -2,8 +2,19 @@
 use Illuminate\Contracts\Events\Dispatcher;
 use XEngine\Signature\Listener;
 use XEngine\Signature\Model;
+use Flarum\Extend;
 
-return function (Dispatcher $events) {
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less')
+        ->route('/t/{slug}', 'tag')
+        ->route('/tags', 'tags'),
+
+        function (Dispatcher $events) {
     $events->subscribe(Listener\AddApplicationAssets::class);
     $events->subscribe(Model\UserSignatureAttributes::class);
 };
+    }
+]
+
