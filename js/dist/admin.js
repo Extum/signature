ed3a172450f5476423668eb054931990f6ac727b
Cody Claborn <cxsquared@gmail.com>
2021-01-08 22:59:26 -0500

    upgrading to 14.1

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -1,2 +1,237 @@
-module.exports=function(t){var e={};function n(i){if(e[i])return e[i].exports;var r=e[i]={i:i,l:!1,exports:{}};return t[i].call(r.exports,r,r.exports,n),r.l=!0,r.exports}return n.m=t,n.c=e,n.d=function(t,e,i){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:i})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var i=Object.create(null);if(n.r(i),Object.defineProperty(i,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)n.d(i,r,function(e){return t[e]}.bind(null,r));return i},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=17)}({0:function(t,e){t.exports=flarum.core.compat.app},1:function(t,e,n){"use strict";function i(t,e){t.prototype=Object.create(e.prototype),t.prototype.constructor=t,t.__proto__=e}n.d(e,"a",(function(){return i}))},13:function(t,e){t.exports=flarum.core.compat["components/SettingsModal"]},17:function(t,e,n){"use strict";n.r(e);var i=n(0),r=n.n(i),a=n(1),o=n(13),s=function(t){function e(){return t.apply(this,arguments)||this}Object(a.a)(e,t);var n=e.prototype;return n.className=function(){return"SignatureSettingsModal Modal--medium"},n.title=function(){return app.translator.trans("xengine-signature.admin.settings.title")},n.form=function(){return[m("div",{className:"Form-group"},m("label",null,app.translator.trans("xengine-signature.admin.settings.maximum_image_width.description")),m("input",{className:"FormControl",placeholder:app.translator.trans("xengine-signature.admin.settings.maximum_image_width.placeholder"),bidi:this.setting("xengine-signature.maximum_image_width")})),m("div",{className:"Form-group"},m("label",null,app.translator.trans("xengine-signature.admin.settings.maximum_image_height.description")),m("input",{className:"FormControl",placeholder:app.translator.trans("xengine-signature.admin.settings.maximum_image_height.placeholder"),bidi:this.setting("xengine-signature.maximum_image_height")})),m("div",{className:"Form-group"},m("label",null,app.translator.trans("xengine-signature.admin.settings.maximum_image_count.description")),m("input",{className:"FormControl",placeholder:app.translator.trans("xengine-signature.admin.settings.maximum_image_count.placeholder"),bidi:this.setting("xengine-signature.maximum_image_count")})),m("div",{className:"Form-group"},m("label",null,app.translator.trans("xengine-signature.admin.settings.maximum_char_limit.description")),m("input",{className:"FormControl",placeholder:app.translator.trans("xengine-signature.admin.settings.maximum_char_limit.placeholder"),bidi:this.setting("xengine-signature.maximum_char_limit")}))]},e}(n.n(o).a);r.a.initializers.add("katosdev-signature",(function(){r.a.extensionSettings["katosdev-signature"]=function(){return r.a.modal.show(new s)}}))}});
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./admin.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./admin.js":
+/*!******************!*\
+  !*** ./admin.js ***!
+  \******************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _src_admin__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/admin */ "./src/admin/index.js");
+/* empty/unused harmony star reexport */
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/inheritsLoose.js":
+/*!******************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/inheritsLoose.js ***!
+  \******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return _inheritsLoose; });
+function _inheritsLoose(subClass, superClass) {
+  subClass.prototype = Object.create(superClass.prototype);
+  subClass.prototype.constructor = subClass;
+  subClass.__proto__ = superClass;
+}
+
+/***/ }),
+
+/***/ "./src/admin/components/SignatureSettingsModal.js":
+/*!********************************************************!*\
+  !*** ./src/admin/components/SignatureSettingsModal.js ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return SignatureSettingsModal; });
+/* harmony import */ var _babel_runtime_helpers_esm_inheritsLoose__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/inheritsLoose */ "./node_modules/@babel/runtime/helpers/esm/inheritsLoose.js");
+/* harmony import */ var flarum_components_SettingsModal__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flarum/components/SettingsModal */ "flarum/components/SettingsModal");
+/* harmony import */ var flarum_components_SettingsModal__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(flarum_components_SettingsModal__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+var SignatureSettingsModal =
+/*#__PURE__*/
+function (_SettingsModal) {
+  Object(_babel_runtime_helpers_esm_inheritsLoose__WEBPACK_IMPORTED_MODULE_0__["default"])(SignatureSettingsModal, _SettingsModal);
+
+  function SignatureSettingsModal() {
+    return _SettingsModal.apply(this, arguments) || this;
+  }
+
+  var _proto = SignatureSettingsModal.prototype;
+
+  _proto.className = function className() {
+    return 'SignatureSettingsModal Modal--medium';
+  };
+
+  _proto.title = function title() {
+    return app.translator.trans('Xengine-signature.admin.settings.title');
+  };
+
+  _proto.form = function form() {
+    return [m("div", {
+      className: "Form-group"
+    }, m("label", null, app.translator.trans('Xengine-signature.admin.settings.maximum_image_width.description')), m("input", {
+      className: "FormControl",
+      placeholder: app.translator.trans('Xengine-signature.admin.settings.maximum_image_width.placeholder'),
+      bidi: this.setting('Xengine-signature.maximum_image_width')
+    })), m("div", {
+      className: "Form-group"
+    }, m("label", null, app.translator.trans('Xengine-signature.admin.settings.maximum_image_height.description')), m("input", {
+      className: "FormControl",
+      placeholder: app.translator.trans('Xengine-signature.admin.settings.maximum_image_height.placeholder'),
+      bidi: this.setting('Xengine-signature.maximum_image_height')
+    })), m("div", {
+      className: "Form-group"
+    }, m("label", null, app.translator.trans('Xengine-signature.admin.settings.maximum_image_count.description')), m("input", {
+      className: "FormControl",
+      placeholder: app.translator.trans('Xengine-signature.admin.settings.maximum_image_count.placeholder'),
+      bidi: this.setting('Xengine-signature.maximum_image_count')
+    })), m("div", {
+      className: "Form-group"
+    }, m("label", null, app.translator.trans('Xengine-signature.admin.settings.maximum_char_limit.description')), m("input", {
+      className: "FormControl",
+      placeholder: app.translator.trans('Xengine-signature.admin.settings.maximum_char_limit.placeholder'),
+      bidi: this.setting('Xengine-signature.maximum_char_limit')
+    }))];
+  };
+
+  return SignatureSettingsModal;
+}(flarum_components_SettingsModal__WEBPACK_IMPORTED_MODULE_1___default.a);
+
+
+
+/***/ }),
+
+/***/ "./src/admin/index.js":
+/*!****************************!*\
+  !*** ./src/admin/index.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var flarum_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! flarum/app */ "flarum/app");
+/* harmony import */ var flarum_app__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_app__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_SignatureSettingsModal__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/SignatureSettingsModal */ "./src/admin/components/SignatureSettingsModal.js");
+
+
+flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.initializers.add('Xengine-signature', function () {
+  flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.extensionSettings['Xengine-signature'] = function () {
+    return flarum_app__WEBPACK_IMPORTED_MODULE_0___default.a.modal.show(new _components_SignatureSettingsModal__WEBPACK_IMPORTED_MODULE_1__["default"]());
+  };
+});
+
+/***/ }),
+
+/***/ "flarum/app":
+/*!********************************************!*\
+  !*** external "flarum.core.compat['app']" ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['app'];
+
+/***/ }),
+
+/***/ "flarum/components/SettingsModal":
+/*!*****************************************************************!*\
+  !*** external "flarum.core.compat['components/SettingsModal']" ***!
+  \*****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['components/SettingsModal'];
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=admin.js.map
